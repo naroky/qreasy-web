a42e8f80b0472c2434ae3b336bb7ae34e945b518
Narupol Balavarnabha <naroky@live.com>
2023-06-13 12:39:33 +0700

    Change node version

--- router/pg_db.js
+++ router/pg_db.js
@@ -1,6 +1,7 @@
 const express = require('express')
-const router = express.Router()
 const pg = require('pg')
+const router = express.Router()
+
 // middleware that is specific to this router
 //router.use((req, res, next) => {
 //console.log('Time: ', Date.now())
